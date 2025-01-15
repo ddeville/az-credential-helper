01765b787582567dff826c68133ca36cc13c9c79
Damien Deville <damien@ddeville.me>
2025-01-15 15:39:34 -0800

    Switch to log

--- cmd/az-blob-credential-helper/main.go
+++ cmd/az-blob-credential-helper/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/url"
 	"os"
 	"strings"
@@ -10,43 +10,38 @@ import (
 	"github.com/ddeville/az-credential-helper/pkg"
 )
 
-const Scope string = "https://storage.azure.com/.default"
-const HostSuffix string = ".blob.core.windows.net"
-
-func fail(msg string) {
-	fmt.Fprintln(os.Stderr, msg)
-	os.Exit(1)
-}
+const scope string = "https://storage.azure.com/.default"
+const hostSuffix string = ".blob.core.windows.net"
 
 func main() {
 	if len(os.Args) < 2 {
-		fail("Missing command")
+		log.Fatal("Missing command")
 	}
 	if strings.TrimSpace(os.Args[1]) != "get" {
-		fail(fmt.Sprintf("Invalid command: %s. Allowed commands: get", os.Args[1]))
+		log.Fatalf("Invalid command: %s. Allowed commands: get", os.Args[1])
 	}
 
 	var req struct {
 		URI string `json:"uri"`
 	}
 	if err := json.NewDecoder(os.Stdin).Decode(&req); err != nil {
-		fail(fmt.Sprintf("Failed to parse JSON: %v", err))
+		log.Fatalf("Failed to parse JSON: %v", err)
 	}
 	if req.URI == "" {
-		fail("Missing uri in input")
+		log.Fatal("Missing uri in input")
 	}
 
 	parsed, err := url.Parse(req.URI)
 	if err != nil {
-		fail(fmt.Sprintf("Invalid uri: %v", err))
+		log.Fatalf("Invalid uri: %v", err)
 	}
-	if !strings.HasSuffix(parsed.Host, HostSuffix) {
-		fail(fmt.Sprintf("Unexpected host in URI: %s. Should be: *%s", parsed.Host, HostSuffix))
+	if !strings.HasSuffix(parsed.Host, hostSuffix) {
+		log.Fatalf("Unexpected host in URI: %s. Should be: *%s", parsed.Host, hostSuffix)
 	}
 
-	accessToken, err := pkg.GetAzureAccessToken(Scope)
+	accessToken, err := pkg.GetAzureAccessToken(scope)
 	if err != nil {
-		fail(fmt.Sprintf("Failed to retrieve token from azure: %v", err))
+		log.Fatalf("Failed to retrieve token from azure: %v", err)
 	}
 
 	res := map[string]map[string][]string{
@@ -57,6 +52,6 @@ func main() {
 	}
 	enc := json.NewEncoder(os.Stdout)
 	if err := enc.Encode(res); err != nil {
-		fail(fmt.Sprintf("Failed to write response: %v", err))
+		log.Fatalf("Failed to write response: %v", err)
 	}
 }
